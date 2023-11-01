eeaaf517d5d18bc40b0e1cbaee5f3a0ef9c34c7e
siva1b3 <siva1b3@gmail.com>
2023-11-01 19:04:54 +0530

    004 part 2

--- reactcourse/src/04 Essentials Deep Dive/App.jsx
+++ reactcourse/src/04 Essentials Deep Dive/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-import App01 from "./01-starting-project/App";
+// import App01 from "./01-starting-project/App";
+// import App02 from "./02 Fragments/App";
+import App03 from "./03 splitting components/App";
 
 function App() {
   return (
     <>
       {/* starting project */}
-      <App01 />
+      {/* <App01 /> */}
+
+      {/* fragments in react */}
+      {/* <App02 /> */}
+
+      {/* splitting components in react */}
+      <App03 />
     </>
   );
 }

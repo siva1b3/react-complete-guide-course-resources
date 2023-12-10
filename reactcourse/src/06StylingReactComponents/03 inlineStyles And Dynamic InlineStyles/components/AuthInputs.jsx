cc1fd2513d893b9031bda45236b9d1abce2fd4d3
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-12-10 14:58:14 +0530

    06 03 start Dynamic & Conditional Inline Styles

---
+++ reactcourse/src/06StylingReactComponents/03 inlineStyles And Dynamic InlineStyles/components/AuthInputs.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === "email") {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes("@");
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  const emailInputJSX = (
+    <label htmlFor="email">
+      <p>Email:</p>
+      <input
+        type="email"
+        id="email"
+        className={emailNotValid ? "invalid" : undefined}
+        aria-labelledby="email"
+        onChange={(event) => handleInputChange("email", event.target.value)}
+      />
+    </label>
+  );
+
+  const passwordInputJSX = (
+    <label htmlFor="password">
+      <p>Password</p>
+      <input
+        id="password"
+        type="password"
+        className={passwordNotValid ? "invalid" : undefined}
+        onChange={(event) => handleInputChange("password", event.target.value)}
+      />
+    </label>
+  );
+
+  const createNewAccountJSX = (
+    <button type="button" className="text-button">
+      Create a new account
+    </button>
+  );
+
+  const signInButtonJSX = (
+    <button type="button" className="button" onClick={handleLogin}>
+      Sign In
+    </button>
+  );
+
+  return (
+    <div id="auth-inputs">
+      <div className="controls">
+        {emailInputJSX}
+        {passwordInputJSX}
+      </div>
+      <div className="actions">
+        {createNewAccountJSX}
+        {signInButtonJSX}
+      </div>
+    </div>
+  );
+}
--- reactcourse/src/06StylingReactComponents/App.jsx
+++ reactcourse/src/06StylingReactComponents/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import App01 from "./01-starting-project/App";
-import App02 from "./02 Splitting CSS Code Across Multiple Files/App";
+// import App02 from "./02 Splitting CSS Code Across Multiple Files/App";
+import App03 from "./03 inlineStyles And Dynamic InlineStyles/App";
 
 function App() {
   return (
@@ -9,7 +10,11 @@ function App() {
       {/* <App01 /> */}
 
       {/* Splitting css between multiple files */}
-      <App02 />
+      {/* <App02 /> */}
+
+      {/* Inline styles */}
+      {/* Dynamic inline styles */}
+      <App03 />
     </>
   );
 }

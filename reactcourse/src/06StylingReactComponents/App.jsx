58dd5af29dd62725b990ba0739ad474f59065e80
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-12-10 17:45:49 +0530

    06 06 112. Introducing "Styled Components" (Third-party Package)

--- reactcourse/src/06StylingReactComponents/App.jsx
+++ reactcourse/src/06StylingReactComponents/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 // import App01 from "./01-starting-project/App";
 // import App02 from "./02 Splitting CSS Code Across Multiple Files/App";
 // import App03 from "./03 inlineStyles And Dynamic InlineStyles/App";
-import App04 from "./04 Dynamic & Conditional Styling with CSS Files & CSS Classes/App";
+// import App04 from "./04 Dynamic & Conditional Styling with CSS Files & CSS Classes/App";
+// import App05 from "./05 Scoping CSS Modules in React/App";
+import App06 from "./06 StyledComponets CSS-IN-JSX/App";
 
 function App() {
   return (
@@ -18,7 +20,14 @@ function App() {
       {/* <App03 /> */}
 
       {/* Dynamic & Conditional Styling with CSS Files & CSS Classes */}
-      <App04 />
+      {/* <App04 /> */}
+
+      {/* Scoping css files using modules */}
+      {/* <App05 /> */}
+
+      {/* CSS on JS */}
+      {/* Using styled componets */}
+      <App06 />
     </>
   );
 }

b72a38d3f37cf011604132f9a3c5bb75945ea966
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-12-11 11:46:57 +0530

    08 working with ref’s and portals started

---
+++ reactcourse/src/08Ref'sAndProtals/01-starting-project/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges" />
+    </>
+  );
+}
+
+export default App;
---
+++ reactcourse/src/08Ref'sAndProtals/01-starting-project/components/Player.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Player() {
+  return (
+    <section id="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button type="button">Set Name</button>
+      </p>
+    </section>
+  );
+}

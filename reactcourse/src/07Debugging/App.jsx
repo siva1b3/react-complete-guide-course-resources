f46f706ff2146e070153a4044cb35474db6f403d
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-12-11 10:05:41 +0530

    07 strated

---
+++ reactcourse/src/07Debugging/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import App01 from "./01-starting-project/App";
+
+function App() {
+  return (
+    <>
+      {/* starter project */}
+      <App01 />
+    </>
+  );
+}
+
+export default App;
--- reactcourse/src/App.jsx
+++ reactcourse/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 // import App03 from "./03ReactBasics/App";
 // import App04 from "./04 Essentials Deep Dive/App";
 // import App05 from "./05ReactEssential-PracticeProject/App";
-import App06 from "./06StylingReactComponents/App";
+// import App06 from "./06StylingReactComponents/App";
+import App07 from "./07Debugging/App";
 
 function App() {
   return (
@@ -17,7 +18,10 @@ function App() {
       {/* <App05 /> */}
 
       {/* Styling react components */}
-      <App06 />
+      {/* <App06 /> */}
+
+      {/* debugging the react code */}
+      <App07 />
     </>
   );
 }

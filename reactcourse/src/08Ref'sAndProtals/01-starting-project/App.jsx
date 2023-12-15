a71678953dbb35122957c08d85539157a86cb868
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-12-15 15:34:29 +0530

    08 01 start

--- reactcourse/src/08Ref'sAndProtals/01-starting-project/App.jsx
+++ reactcourse/src/08Ref'sAndProtals/01-starting-project/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Root from "./Root";
 
 import Player from "./components/Player";
 import "./index.css";
 
 function App() {
   return (
-    <>
+    <Root>
       <Player />
       <div id="challenges" />
-    </>
+    </Root>
   );
 }
 
---
+++ reactcourse/src/08Ref'sAndProtals/01-starting-project/Root.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Root(props) {
+  const { children } = props;
+  return (
+    <>
+      <div id="modal" />
+      <div id="content">
+        <header>
+          <h1>
+            The
+            <em>Almost</em>
+            Final Countdown
+          </h1>
+          <p>Stop the timer once you estimate that time is (almost) up</p>
+        </header>
+        <div id="root">{children}</div>
+      </div>
+    </>
+  );
+}
+
+export default Root;

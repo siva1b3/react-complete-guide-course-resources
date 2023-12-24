cfacb566610beb502531555c3f156139b4bcb40a
SivaDev <42565813+siva1b3@users.noreply.github.com>
2023-12-24 19:55:04 +0530

    08Ref'sAndProtals/03 UseRef Intro Started

--- reactcourse/src/08Ref'sAndProtals/App.jsx
+++ reactcourse/src/08Ref'sAndProtals/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 // import App01 from "./01-starting-project/App";
-import App02 from "./02 Basic UseState Example/App";
+// import App02 from "./02 Basic UseState Example/App";
+import App03 from "./03 UseRef Intro/App";
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       {/* <App01 /> */}
 
       {/* Basic example of use state */}
-      <App02 />
+      {/* <App02 /> */}
+
+      {/* Use Ref Intro */}
+      <App03 />
     </>
   );
 }
